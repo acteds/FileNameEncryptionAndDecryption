8ae8a46a4ea34467c91de1f2257830023f666f38
acted <1285567505@qq.com>
2021-04-07 07:15:08 +0800

    更新README,添加重新编译批处理

---
+++ 重新编译JAVA.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+mode con lines=20 cols=90
+color 0a
+title %~n0
+setlocal enabledelayedexpansion
+cd /d "%~dp0"
+cd src
+
+rem javac -cp .;commons-io-2.8.0.jar;fastjson-1.2.75.jar -encoding UTF-8 -d . 生成本地文件记录加密定制版.java
+rem javac -cp .;fastjson-1.2.75.jar -encoding UTF-8 -d . 文件名称加密解密.java
+rem javac -cp .;fastjson-1.2.75.jar -encoding UTF-8 -d . 通过JSON精确加密.java
+echo 已完成编译&pause
