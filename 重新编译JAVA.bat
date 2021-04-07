debd939dc3906e0bbd6e3058bdc0c6395ddf2a3a
acted <1285567505@qq.com>
2021-04-07 08:30:53 +0800

    修复

--- 重新编译JAVA.bat
+++ 重新编译JAVA.bat
@@ -6,7 +6,7 @@ setlocal enabledelayedexpansion
 cd /d "%~dp0"
 cd src
 
-rem javac -cp .;commons-io-2.8.0.jar;fastjson-1.2.75.jar -encoding UTF-8 -d . 生成本地文件记录加密定制版.java
-rem javac -cp .;fastjson-1.2.75.jar -encoding UTF-8 -d . 文件名称加密解密.java
-rem javac -cp .;fastjson-1.2.75.jar -encoding UTF-8 -d . 通过JSON精确加密.java
+javac -cp .;commons-io-2.8.0.jar;fastjson-1.2.75.jar -encoding UTF-8 -d . 生成本地文件记录加密定制版.java
+javac -cp .;fastjson-1.2.75.jar -encoding UTF-8 -d . 文件名称加密解密.java
+javac -cp .;fastjson-1.2.75.jar -encoding UTF-8 -d . 通过JSON精确加密.java
 echo 已完成编译&pause
